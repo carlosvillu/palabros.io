3c1c39eca8ca90492f488ad071c8f272199e6790
Carlos Villuendas Zambrana <carlosvillu@gmail.com>
2022-11-14 10:28:34 +0100

    chore(META): add function

--- functions/api/definitions/[word].ts
+++ functions/api/definitions/[word].ts
@@ -1,19 +1,14 @@
-interface Env {
-  NODE_VERSION: string
-}
-
-export const onRequestGet: PagesFunction<Env> = async (context) => {
+export const onRequestGet: PagesFunction<unknown> = async (context) => {
   // Contents of context object
   const {
-    request // same as existing Worker API
+    // request, // same as existing Worker API
     // env, // same as existing Worker API
-    // params, // if filename includes [id] or [[path]]
+    params // if filename includes [id] or [[path]]
     // waitUntil, // same as ctx.waitUntil in existing Worker API
     // passThroughOnException, // same as ctx.passThroughOnException in existing Worker API
     // next, // used for middleware or to fetch assets
     // data // arbitrary space for passing data between middlewares
   } = context
 
-  console.log(request.url)
-  return new Response('Hello, world!')
+  return await fetch(`https://dle.rae.es/${params.word as string}`).then(async resp => await resp.text())
 }
