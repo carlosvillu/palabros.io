db3c52a8cb1989099466974081b022c5d0dd3c1a
David Garcia <dave74@gmail.com>
2022-12-04 11:34:53 +0100

    header css modules

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,11 @@
 import { ReactElement } from 'react'
-import './index.css'
+import styles from './index.module.css'
 import Hamburger from '../Icons/Hamburguer'
 
 function Header (): ReactElement {
-  return <header className="Header debug">
+  return <header className={styles.header}>
     <div className="Header-Left"><h1 className="Header-Logo-Container">P</h1></div>
-    <div className="Header-Rigth"><span className="Header-Menu-Container debug"><Hamburger /></span></div>
+    <div className="Header-Rigth"><span className={styles.headerMenuContainer}><Hamburger /></span></div>
   </header >
 }
 
