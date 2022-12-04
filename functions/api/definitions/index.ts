1e4f3ef7b348e04599dba3c7ae987535b6a05603
Carlos Villuendas Zambrana <carlosvillu@gmail.com>
2022-12-04 10:18:11 +0100

    chore(META): add css reset

--- functions/api/definitions/index.ts
+++ functions/api/definitions/index.ts
@@ -3,6 +3,7 @@ interface Env {
 }
 
 export const onRequest: PagesFunction<Env> = async (context) => {
+  debugger // eslint-disable-line 
   // Contents of context object
   const {
     request // same as existing Worker API
